91675689ac55a12d6989c1e56da056d4befbae47
Miko Chu <angelomycar@gmail.com>
2021-01-21 13:41:20 +0800

    fixed incorrect routing

--- src/api_web/routes/book-route.js
+++ src/api_web/routes/book-route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const book_controller = require('../controllers/book-controller')
 
 router.get('/', book_controller.all)
-router.get('/detail/:id', book_controller.get)
+router.get('/detail/:id', book_controller.detail)
 router.post('/book/create', book_controller.create)
 router.patch('book/update/:id', book_controller.update)
 router.delete('book/delete/:id', book_controller.delete)
