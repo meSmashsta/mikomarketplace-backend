a8166f9655805c2db91c3d032d09fa663545478c
Miko Chu <angelomycar@gmail.com>
2021-01-21 09:49:15 +0800

    defined db connection

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -1,1 +1,12 @@
-// TODO: add mongodb connection here
+const mongoose = require('mongoose')
+const { MONGO_HOST, MONGO_USERNAME, MONGO_PASSWORD, MONGO_PORT, MONGO_DATABASE } = require('../../../config')
+
+module.exports = function makeDB() {
+    const credential = `${MONGO_USERNAME}:${encodeURIComponent(MONGO_PASSWORD)}`
+
+    const mongoURI = `mongodb://${credential}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DATABASE}`
+
+    return async function db() {
+        return await mongoose.connect(mongoURI)
+    }
+}
